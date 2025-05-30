e9aa9a7b3208dbdeae04acee2d9ca18d5632b4b3
Dragy Devi <dragydevi@gmail.com>
2025-05-30 15:11:01 -0400

    default is false

--- Content.Shared/_Harmony/CCVars/HCCVars.cs
+++ Content.Shared/_Harmony/CCVars/HCCVars.cs
@@ -18,5 +18,5 @@ public sealed class HCCVars
     /// Allows server hosters to turn the queue on and off
     /// </summary>
     public static readonly CVarDef<bool> EnableQueue =
-        CVarDef.Create("harmony.enable_queue", true, CVar.SERVER);
+        CVarDef.Create("harmony.enable_queue", false, CVar.SERVER);
 }
